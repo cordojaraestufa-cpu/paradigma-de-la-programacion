4f03f4678e8eb6cfff5fcf9f65ce8b5dfae332e9
cordojaraestufa-cpu <cordojaraestefa@gmail.com>
2026-02-25 16:11:55 -0500

    codigos secuenciales

---
+++ programacion_secuencial.js
@@ -0,0 +1,21 @@
+let nombre = "rocio";
+let edad = 25;
+let apellido = "alvarez";
+let numero =29;
+
+function sumar(a, b) {
+    return a + b;
+}
+
+function restar(a, b){
+    let operacion = a - b;
+    return operacion;
+}
+
+
+console.log("el resultado de la suma es: " + sumar(numero,edad));
+let resultadoresta = restar(5 , 7);
+
+console.log("resultado de la resta: " + resultadoresta);
+console.log("nombre completo: " + nombre, + "" + apellido, + ", edad: " + edad);
+ 
